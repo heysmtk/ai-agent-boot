5ee6279ebe5e635f32830704916a67cbb41c5f2a
heysmtk <smtktom@gmail.com>
2025-12-23 13:31:00 +0100

    add user input via argparse

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,7 +15,7 @@ if api_key is None:
     raise RuntimeError("API KEY wasn't found.")
 
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model='gemini-2.5-flash', contents=args.user_prompt
 )
 
 prompt_tokens_count = response.usage_metadata.prompt_token_count
@@ -19,3 +24,4 @@ response_tokens_count = response.usage_metadata.candidates_token_count
 print(f"Prompt tokens: {prompt_tokens_count}")   
 print(f"Response tokens: {response_tokens_count}")
 print(response.text)
+
