93657fb6bf953a3aad0588bb3431dd2425296ea2
heysmtk <smtktom@gmail.com>
2025-12-23 13:05:04 +0100

    add API KEY, raise runtime error message and creating response

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from ai-agent-boot!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
 
-if __name__ == "__main__":
-    main()
+if api_key is None:
+    raise RuntimeError("API KEY wasn't found.")
+
+response = client.models.generate_content(
+    model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+)
+
+print(response.text)
