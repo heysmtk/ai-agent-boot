f95dd9f23ab9c2fd754ff4f2a3465a621fa12d20
heysmtk <smtktom@gmail.com>
2026-01-06 22:13:29 +0100

    add --verbose argument for showing metadata

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -22,10 +23,13 @@ response = client.models.generate_content(
     model='gemini-2.5-flash', contents=messages
 )
 
+user_prompt = args.user_prompt
 prompt_tokens_count = response.usage_metadata.prompt_token_count
 response_tokens_count = response.usage_metadata.candidates_token_count
 
-print(f"Prompt tokens: {prompt_tokens_count}")   
-print(f"Response tokens: {response_tokens_count}")
-print(response.text)
-
+if args.verbose: 
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {prompt_tokens_count}")   
+    print(f"Response tokens: {response_tokens_count}")
+else:
+    print(response.text)
